b27a8831bfcb754057bdc7f7e12270e21f196ba1
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2021-06-11 15:16:27 +0100

    Express and MongoDB Setup

---
+++ config/db.js
@@ -0,0 +1,25 @@
+//bring in mongoose to connect
+const mongoose = require('mongoose');
+
+//grab the string from the config package
+const config = require('config');
+
+//get the config value
+const db = config.get('mongoURI');
+
+//async/await function to connect
+const connectDB = async () => {
+  //try to connect. Await because it returns a promise
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
+    console.log('MongoDB connected...');
+    //if can't connect, show error message and break
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+//export the connectDB function
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 //bring in express
 const express = require('express');
 
+//add the DB connection
+const connectDB = require('./config/db.js');
+
 //initialise app variable with express
 const app = express();
 
+//call the DB connection and connect
+connectDB();
+
 //single end point to test and send something to the browser
 app.get('/', (req, res) => res.send('API running'));
+
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 //looks for an environment variable in Heroku else runs locally on 5000
 const PORT = process.env.PORT || 5000;
