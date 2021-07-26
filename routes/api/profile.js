b028a8f903ffeead4844788dce928e58fe855d5f
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2021-07-26 15:35:54 +0100

    Edit profile and dashboard actions

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -67,7 +67,7 @@ router.post(
     const profileFields = {};
     profileFields.user = req.user.id;
     if (company) profileFields.company = company;
-    if (website) profileFields.website = company;
+    if (website) profileFields.website = website;
     if (location) profileFields.location = location;
     if (bio) profileFields.bio = bio;
     if (status) profileFields.status = status;
