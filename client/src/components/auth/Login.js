b2687bfc5b083c60a34c96885700f2d4fd93d902
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2021-09-02 13:45:56 +0100

    fixes

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 const Login = () => {
   const [formData, setFormData] = useState({
     email: '',
+    password: '',
   });
 
   const dispatch = useDispatch();
